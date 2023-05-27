033bc97f99309989b4a56f5842e85158ebf3647c
Oleg Barbashow <teosofio@gmail.com>
2023-05-27 15:58:33 +0300

    Added RandomChar module

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './components/app/App';
-import MarvelService from "./components/services/MarvelService";
 import './style/style.scss';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
-
-const marvelService = new MarvelService();
-
-marvelService.getAllCharacters().then(res=>res.data.results.forEach(item => console.log(item.name)));
 
 root.render(
   <StrictMode>
